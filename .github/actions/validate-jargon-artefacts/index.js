4ea1b61bba763c6f09ca9e2f1eee0d9afcf42f9d
Huy Nguyen <huy.nguyenkim.h@gmail.com>
2025-02-12 17:08:19 +0700

    enhance validation logic to check for artefacts in Jargon payload [skip ci]

--- .github/actions/validate-jargon-artefacts/index.js
+++ .github/actions/validate-jargon-artefacts/index.js
@@ -14,7 +14,7 @@ const jsonInstanceSuffix = '_instance_jsonSchema.json';
 
 async function validateJargonArtefacts(jargonArtefact) {
   try {
-    if (!jargonArtefact) {
+    if (!jargonArtefact || !jargonArtefact.artefacts) {
       return core.setFailed('No Jargon artefact payload found.');
     }
     if (jargonArtefact.action && jargonArtefact.action.test) {
@@ -22,7 +22,8 @@ async function validateJargonArtefacts(jargonArtefact) {
     }
 
     core.info('Validating Jargon artefacts...');
-    if (jargonArtefact.jsonSchemas && jargonArtefact.jsonSchemas.length) {
+    core.info(`Json Schemas: ${jargonArtefact.artefacts.jsonSchemas}`);
+    if (jargonArtefact.artefacts.jsonSchemas && jargonArtefact.artefacts.jsonSchemas.length) {
       core.info('Validating sample credentials...');
       await validateSampleCredentials(jargonArtefact.jsonSchemas);
       core.info('Sample credentials validation complete.');
